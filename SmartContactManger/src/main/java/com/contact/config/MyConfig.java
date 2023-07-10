6030ec66d653ddf87c18c0a5e228b57988cbb61a
AnkeshSofDev <139131367+AnkeshSofDev@users.noreply.github.com>
2023-07-10 17:32:09 +0530

    Add files via upload

---
+++ SmartContactManger/src/main/java/com/contact/config/MyConfig.java
@@ -0,0 +1,10 @@
+package com.contact.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfig {
+	
+	
+
+}
---
+++ SmartContactManger/src/main/java/com/contact/entities/User.java
@@ -0,0 +1,120 @@
+package com.contact.entities;
+
+import java.util.ArrayList;
+
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "USER")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@NotBlank(message = "Name field is require!!")
+	@Size(min = 2,max = 20,message = "Min 2 and Max 2o digits are allowed!!")
+	private String name;
+	@Column(unique = true)
+	private String email;
+	private String password;
+	private String role;
+	private boolean enabled;
+	private String imageUrlString;
+	@Column(length = 500)
+	private String about;
+	
+	@OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY,mappedBy = "user")
+	private List<Contact> contacts = new ArrayList<>();
+	
+	public User() {
+		super();
+	}
+	public User(int id, String name, String email, String password, String role, boolean enabled, String imageUrlString,
+			String about) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+		this.enabled = enabled;
+		this.imageUrlString = imageUrlString;
+		this.about = about;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public String getImageUrlString() {
+		return imageUrlString;
+	}
+	public void setImageUrlString(String imageUrlString) {
+		this.imageUrlString = imageUrlString;
+	}
+	public String getAbout() {
+		return about;
+	}
+	public void setAbout(String about) {
+		this.about = about;
+	}
+	public List<Contact> getContacts() {
+		return contacts;
+	}
+	public void setContacts(List<Contact> contacts) {
+		this.contacts = contacts;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", role=" + role
+				+ ", enabled=" + enabled + ", imageUrlString=" + imageUrlString + ", about=" + about + ", contacts="
+				+ contacts + "]";
+	}
+	
+	
+
+}
